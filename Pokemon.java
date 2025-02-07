9bbf785633bcf4206902f7ff58dbea862e35c3dc
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-02-07 13:52:13 -0600

    Add method to display Pokemon attributes upon initialization

--- Pokemon.java
+++ Pokemon.java
@@ -7,5 +7,12 @@ public class Pokemon {
         this.nombre = nombre;
         this.tipo = tipo;
         this.habilidad = habilidad;
+        this.mostrarDatos();
+    }
+
+    public void mostrarDatos() {
+        System.out.println("Nombre: " + this.nombre);
+        System.out.println("Tipo: " + this.tipo);
+        System.out.println("Habilidad: " + this.habilidad);
     }
 }
