fa425ae8d290d82920974216541d45e4e4eb9bfd
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-02-09 13:51:39 -0600

    Add pokemons to the ArrayList

--- Main.java
+++ Main.java
@@ -4,6 +4,12 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Pokemon> pokedex = new ArrayList<>(); // Aquí se crea el ArrayList de Pokemones
 
-        pokedex.add(new Pokemon(null, null, null)); // Aquí se crea un nuevo Pokemon (Cambiar null por los valores que quieras)
+        // pokedex.add(new Pokemon(null, null, null)); // Aquí se crea un nuevo Pokemon (Cambiar null por los valores que quieras)
+        pokedex.add(new Pokemon("Pikachu", "Eléctrico", "Electricidad Estática"));
+        pokedex.add(new Pokemon("Charmander", "Fuego", "Mar de Llamas"));
+        pokedex.add(new Pokemon("Bulbasaur", "Planta/Veneno", "Espesura"));
+        pokedex.add(new Pokemon("Squirtle", "Agua", "Torrente"));
+        pokedex.add(new Pokemon("Eevee", "Normal", "Fuga"));
+        pokedex.add(new Pokemon("Gengar", "Fantasma/Veneno", "Levitación"));
     }
 }
