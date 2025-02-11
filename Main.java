6bce2353ed2969df161e6b9013c157ea9c105243
SergioAAC <andresbiki2002@gmail.com>
2025-02-11 12:31:06 -0600

    Cambio a enteros

--- Main.java
+++ Main.java
@@ -7,12 +7,12 @@ public class Main {
         ArrayList<Pokemon> pokedex = new ArrayList<>(); // Aquí se crea el ArrayList de Pokemones
 
         // pokedex.add(new Pokemon(null, null, null)); // Aquí se crea un nuevo Pokemon (Cambiar null por los valores que quieras)
-        pokedex.add(new Pokemon(101, "Pikachu", "Eléctrico", "Electricidad Estática", 120.50, 5));
-        pokedex.add(new Pokemon(102, "Charmander", "Fuego", "Mar de Llamas", 150.75, 5));
-        pokedex.add(new Pokemon(103, "Bulbasaur", "Planta/Veneno", "Espesura", 130.00, 5));
-        pokedex.add(new Pokemon(104, "Squirtle", "Agua", "Torrente", 140.25, 5));
-        pokedex.add(new Pokemon(105, "Eevee", "Normal", "Fuga", 160.00, 5));
-        pokedex.add(new Pokemon(106, "Gengar", "Fantasma/Veneno", "Levitación", 200.00, 5));
+        pokedex.add(new Pokemon(101, "Pikachu", "Eléctrico", "Electricidad Estática", 120, 5));
+        pokedex.add(new Pokemon(102, "Charmander", "Fuego", "Mar de Llamas", 150, 5));
+        pokedex.add(new Pokemon(103, "Bulbasaur", "Planta/Veneno", "Espesura", 130, 5));
+        pokedex.add(new Pokemon(104, "Squirtle", "Agua", "Torrente", 140, 5));
+        pokedex.add(new Pokemon(105, "Eevee", "Normal", "Fuga", 160, 5));
+        pokedex.add(new Pokemon(106, "Gengar", "Fantasma/Veneno", "Levitación", 200, 5));
 
         while (true) {
             System.out.println("Stock de Pokémon disponible:");
