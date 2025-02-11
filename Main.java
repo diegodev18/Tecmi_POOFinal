326c610454352506669017ddbbf7f9e82bf3960d
SergioAAC <andresbiki2002@gmail.com>
2025-02-11 07:15:29 -0600

    Se agrego la cantidad de pokemons

--- Main.java
+++ Main.java
@@ -7,17 +7,17 @@ public class Main {
         ArrayList<Pokemon> pokedex = new ArrayList<>(); // Aquí se crea el ArrayList de Pokemones
 
         // pokedex.add(new Pokemon(null, null, null)); // Aquí se crea un nuevo Pokemon (Cambiar null por los valores que quieras)
-        pokedex.add(new Pokemon(101, "Pikachu", "Eléctrico", "Electricidad Estática", 120.50));
-        pokedex.add(new Pokemon(102, "Charmander", "Fuego", "Mar de Llamas", 150.75));
-        pokedex.add(new Pokemon(103, "Bulbasaur", "Planta/Veneno", "Espesura", 130.00));
-        pokedex.add(new Pokemon(104, "Squirtle", "Agua", "Torrente", 140.25));
-        pokedex.add(new Pokemon(105, "Eevee", "Normal", "Fuga", 160.00));
-        pokedex.add(new Pokemon(106, "Gengar", "Fantasma/Veneno", "Levitación", 200.00));
+        pokedex.add(new Pokemon(101, "Pikachu", "Eléctrico", "Electricidad Estática", 120.50, 5));
+        pokedex.add(new Pokemon(102, "Charmander", "Fuego", "Mar de Llamas", 150.75, 5));
+        pokedex.add(new Pokemon(103, "Bulbasaur", "Planta/Veneno", "Espesura", 130.00, 5));
+        pokedex.add(new Pokemon(104, "Squirtle", "Agua", "Torrente", 140.25, 5));
+        pokedex.add(new Pokemon(105, "Eevee", "Normal", "Fuga", 160.00, 5));
+        pokedex.add(new Pokemon(106, "Gengar", "Fantasma/Veneno", "Levitación", 200.00, 5));
 
         while (true) {
             System.out.println("Stock de Pokémon disponible:");
             for (Pokemon p : pokedex) {
-                System.out.println(p.nombre + " - " + p.codigo + " - " + p.precio);
+                System.out.println(p.NOMBRE + " - " + p.CODIGO + " - " + p.PRECIO);
             }
             System.out.println("\nPresione [" + AnsiColors.RED.TXT + "0" + AnsiColors.RESET + "] para salir");
             System.out.print(AnsiColors.BLUE.TXT + "Ingrese el código: ");
@@ -39,7 +39,7 @@ public class Main {
                 int pago_maquina = scanner.nextInt();
                 scanner.nextLine();
 
-                if (pago_maquina < encontrado.precio) {
+                if (pago_maquina < encontrado.PRECIO) {
                     System.out.println(AnsiColors.RED.TXT + "Falta dinero!");
                 } else {
                     System.out.println(AnsiColors.YELLOW.TXT + "Pago completado!");
